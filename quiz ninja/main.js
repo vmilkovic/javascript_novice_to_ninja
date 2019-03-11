4f163376b2b74eff714a40a356204035cb194513
Miillky <vedran.milkovic25@gmail.com>
2019-03-11 20:25:01 +0100

    Stao na Retrieving and Changing Values from a Form

--- quiz ninja/main.js
+++ quiz ninja/main.js
@@ -4,8 +4,31 @@ const quiz = [
     { name: "Batman", realName: "Bruce Wayne"}
 ];
 
+// View Object
+
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    start: document.getElementById('start'),
+    render(target, content, attributes){
+        for(const key in attributes){
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    },
+    show(element){
+        element.style.display = 'block';
+    },
+    hide(element){
+        element.style.display = 'none';
+    }
+};
+
 const game = {
     start(quiz){
+        view.hide(view.start);
         this.questions = [...quiz];
         this.score = 0;
         // main game loop
@@ -18,21 +41,26 @@ const game = {
     },
     ask(){
         const question = `What is ${this.question.name}'s real name?`;
+        view.render(view.question, question);
         const response = prompt(question);
         this.check(response);
     },
     check(response){
         const answer = this.question.realName;
         if(response === answer){
+            view.render(view.result, 'Correct!', {'class':'correct'})
             alert('Correct!');
             this.score++;
+            view.render(view.score, this.score);
         } else {
+            view.render(view.result, `Wrong! The correct answer was ${answer}`, {'class': 'wrong'})
             alert(`Wrong! The correct answer was ${answer}`);
         }
     },
     gameOver(){
-        alert(`Game Over, you score ${this.score} point${this.score !== 1 ? 's' : ''}`);
+        view.show(view.start);
+        view.render(view.info, `Game Over, you score ${this.score} point${this.score !== 1 ? 's' : ''}`)
     }
 }
 
-game.start(quiz);
+view.start.addEventListener('click', () => game.start(quiz), false);
--- test.js
+++ test.js
@@ -1,16 +1,6 @@
-const myMaths = {
+const form = document.forms['search'];
+form.addEventListener ('submit', search, false)
 
-	square(x){
-		return x * x;
-	},
-	mean(array, callback){
-		if(callback){
-			array.map(callback);
-		}
-		const total = array.reduce((a, b) => a + b);
-		return total/array.length;
-	}
-};
-
-console.log(myMaths.square(3));
-console.log(myMaths.mean([1,2,3]));
+function search() {
+alert(' Form Submitted');
+}
